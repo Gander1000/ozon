8dc6079a28c0a0d44f4bb292b2e95ccbdc5e4c6d
Aldos650 <tynctykanarbekov918@gmail.com>
2025-04-02 15:03:27 +0600

    fotal

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import CartFo from './component/CartFo/CartFo'
 import Logo from './component/Logo/Logo'
 import CartHeder from './component/CartHeder/CartHeder'
 import Foter from './component/Foter/Foter'
-import Sunshine from "./component/sunshine/Sunshine";
+import Sunshine from "./component/Sunshine/Sunshine"
 
 
 
